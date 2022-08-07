eeff87bc89eaffb9c856403ec5299bb7da064234
Sinisa Koscec <snsa_kscc@asdf.local>
2022-08-07 03:28:12 +0200

    second

---
+++ components/Layout.js
@@ -0,0 +1,85 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+const Header = () => {
+  const { asPath } = useRouter();
+
+  return (
+    <header className="text-gray-700 body-font">
+      <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+        <Link href="/">
+          <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-10 h-10 p-2 text-white bg-red-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">dvasadva</span>
+          </a>
+        </Link>
+        <nav className="flex flex-wrap items-center justify-center space-x-4 text-base md:ml-auto">
+          <Link href="/about">
+            <a className={asPath === "/about" ? "font-black text-black" : "hover:text-gray-900"}>About Us</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+const Footer = () => (
+  <footer className="text-gray-700 body-font">
+    <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+      <div className="flex-shrink-0 w-64 mx-auto mt-10 text-center md:mx-0 md:text-left md:mt-0">
+        <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 p-2 text-white bg-red-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">dvasadva d.o.o.</span>
+        </a>
+        <p className="mt-2 text-sm text-gray-500">Motion graphics agency harnesing power of deep neural networks.</p>
+      </div>
+    </div>
+    <div className="bg-gray-200">
+      <div className="container flex flex-col flex-wrap px-5 py-4 mx-auto sm:flex-row">
+        <p className="text-sm text-center text-gray-500 sm:text-left">© 2022 dvasadva d.o.o.</p>
+        <span className="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+          <a className="ml-3 text-gray-500">
+            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+              <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+        </span>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { AnimatePresence } from "framer-motion";
+import React from "react";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  const [isFirstMount, setIsFirstMount] = React.useState(true);
+
+  React.useEffect(() => {
+    const handleRouteChange = () => {
+      isFirstMount && setIsFirstMount(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  });
+
+  return (
+    <Layout>
+      <AnimatePresence exitBeforeEnter>
+        <Component isFirstMount={isFirstMount} key={router.route} {...pageProps} />
+      </AnimatePresence>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
