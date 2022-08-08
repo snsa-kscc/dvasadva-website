9b9a5d7ab3aeaf0843e9ac84d1d2e202cf376d08
Sinisa Koscec <snsa_kscc@asdf.local>
2022-08-08 04:13:04 +0200

    about copy

--- pages/about.js
+++ pages/about.js
@@ -37,89 +37,53 @@ const ContactPage = () => {
         <motion.div variants={title} className="flex flex-col w-full mb-12">
           <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font text-center">About Us</h1>
           <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere urna nec
-            tincidunt praesent semper feugiat nibh sed pulvinar. Orci dapibus ultrices in iaculis. Lorem ipsum dolor sit amet consectetur adipiscing elit ut.
-            Dictum varius duis at consectetur lorem donec massa sapien faucibus. Massa tempor nec feugiat nisl pretium fusce. Sed arcu non odio euismod lacinia
-            at quis. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.
-            Id faucibus nisl tincidunt eget nullam non nisi est sit. Donec massa sapien faucibus et molestie ac feugiat. Nunc vel risus commodo viverra maecenas
-            accumsan lacus. Arcu bibendum at varius vel pharetra vel turpis. Ultrices gravida dictum fusce ut placerat orci nulla. Habitant morbi tristique
-            senectus et netus. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Vel fringilla est ullamcorper eget nulla facilisi. Felis eget
-            velit aliquet sagittis id consectetur purus ut faucibus. Enim diam vulputate ut pharetra sit amet. Pulvinar proin gravida hendrerit lectus. Lectus
-            vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Convallis
-            posuere morbi leo urna molestie at elementum eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Fames ac turpis egestas sed
-            tempus urna et pharetra. Ullamcorper a lacus vestibulum sed. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Sapien eget mi proin sed
-            libero. Viverra aliquet eget sit amet tellus cras adipiscing enim. Augue eget arcu dictum varius duis. Donec ultrices tincidunt arcu non sodales
-            neque sodales ut etiam. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Nunc eget lorem dolor sed. Iaculis eu non diam phasellus
-            vestibulum lorem sed risus. Tincidunt tortor aliquam nulla facilisi cras fermentum. Hac habitasse platea dictumst vestibulum rhoncus. Accumsan lacus
-            vel facilisis volutpat. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Nibh ipsum consequat nisl vel. Congue quisque
-            egestas diam in arcu cursus euismod quis viverra. Amet luctus venenatis lectus magna fringilla urna. Porta non pulvinar neque laoreet suspendisse
-            interdum. Sit amet justo donec enim diam vulputate ut pharetra sit. Mauris commodo quis imperdiet massa tincidunt. Consequat interdum varius sit
-            amet. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. In nibh mauris cursus mattis. Cras semper auctor neque vitae tempus. Vitae
-            suscipit tellus mauris a diam maecenas. Libero justo laoreet sit amet. Adipiscing diam donec adipiscing tristique risus nec feugiat in. Etiam sit
-            amet nisl purus in. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Ornare aenean euismod elementum nisi quis eleifend quam
-            adipiscing. Magna ac placerat vestibulum lectus. Convallis convallis tellus id interdum velit. Malesuada proin libero nunc consequat interdum. Erat
-            pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Arcu dictum varius duis at consectetur lorem. Sodales ut etiam sit amet nisl
-            purus in mollis nunc. Aliquet eget sit amet tellus cras adipiscing. Amet mattis vulputate enim nulla aliquet porttitor. Eros donec ac odio tempor
-            orci dapibus. Id porta nibh venenatis cras sed felis eget velit aliquet. Nulla pharetra diam sit amet nisl suscipit. Risus sed vulputate odio ut
-            enim blandit volutpat. Elementum integer enim neque volutpat ac tincidunt. Auctor neque vitae tempus quam pellentesque nec nam. In iaculis nunc sed
-            augue lacus viverra vitae congue eu.
+            Ok, we all know how it goes. You land on a website of some random business on the Internet run by some random gals and guys and immediately you are
+            slapped by fancy marketing gibberish buzz words such as <i>experiences, rich & digital</i> and of course given the fact we live in 2022
+            <i> machine learning & blazingly fast</i> has to be involved, right?
           </p>
           <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere urna nec
-            tincidunt praesent semper feugiat nibh sed pulvinar. Orci dapibus ultrices in iaculis. Lorem ipsum dolor sit amet consectetur adipiscing elit ut.
-            Dictum varius duis at consectetur lorem donec massa sapien faucibus. Massa tempor nec feugiat nisl pretium fusce. Sed arcu non odio euismod lacinia
-            at quis. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.
-            Id faucibus nisl tincidunt eget nullam non nisi est sit. Donec massa sapien faucibus et molestie ac feugiat. Nunc vel risus commodo viverra maecenas
-            accumsan lacus. Arcu bibendum at varius vel pharetra vel turpis. Ultrices gravida dictum fusce ut placerat orci nulla. Habitant morbi tristique
-            senectus et netus. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Vel fringilla est ullamcorper eget nulla facilisi. Felis eget
-            velit aliquet sagittis id consectetur purus ut faucibus. Enim diam vulputate ut pharetra sit amet. Pulvinar proin gravida hendrerit lectus. Lectus
-            vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Convallis
-            posuere morbi leo urna molestie at elementum eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Fames ac turpis egestas sed
-            tempus urna et pharetra. Ullamcorper a lacus vestibulum sed. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Sapien eget mi proin sed
-            libero. Viverra aliquet eget sit amet tellus cras adipiscing enim. Augue eget arcu dictum varius duis. Donec ultrices tincidunt arcu non sodales
-            neque sodales ut etiam. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Nunc eget lorem dolor sed. Iaculis eu non diam phasellus
-            vestibulum lorem sed risus. Tincidunt tortor aliquam nulla facilisi cras fermentum. Hac habitasse platea dictumst vestibulum rhoncus. Accumsan lacus
-            vel facilisis volutpat. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Nibh ipsum consequat nisl vel. Congue quisque
-            egestas diam in arcu cursus euismod quis viverra. Amet luctus venenatis lectus magna fringilla urna. Porta non pulvinar neque laoreet suspendisse
-            interdum. Sit amet justo donec enim diam vulputate ut pharetra sit. Mauris commodo quis imperdiet massa tincidunt. Consequat interdum varius sit
-            amet. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. In nibh mauris cursus mattis. Cras semper auctor neque vitae tempus. Vitae
-            suscipit tellus mauris a diam maecenas. Libero justo laoreet sit amet. Adipiscing diam donec adipiscing tristique risus nec feugiat in. Etiam sit
-            amet nisl purus in. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Ornare aenean euismod elementum nisi quis eleifend quam
-            adipiscing. Magna ac placerat vestibulum lectus. Convallis convallis tellus id interdum velit. Malesuada proin libero nunc consequat interdum. Erat
-            pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Arcu dictum varius duis at consectetur lorem. Sodales ut etiam sit amet nisl
-            purus in mollis nunc. Aliquet eget sit amet tellus cras adipiscing. Amet mattis vulputate enim nulla aliquet porttitor. Eros donec ac odio tempor
-            orci dapibus. Id porta nibh venenatis cras sed felis eget velit aliquet. Nulla pharetra diam sit amet nisl suscipit. Risus sed vulputate odio ut
-            enim blandit volutpat. Elementum integer enim neque volutpat ac tincidunt. Auctor neque vitae tempus quam pellentesque nec nam. In iaculis nunc sed
-            augue lacus viverra vitae congue eu.
+            Hell, I would have thought exactly the same & I really strive to live by the principle of finding niceties in most of situations. At least I'm
+            trying, I'm trying <i>Ringo to be the good shepherd.</i>
           </p>
           <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere urna nec
-            tincidunt praesent semper feugiat nibh sed pulvinar. Orci dapibus ultrices in iaculis. Lorem ipsum dolor sit amet consectetur adipiscing elit ut.
-            Dictum varius duis at consectetur lorem donec massa sapien faucibus. Massa tempor nec feugiat nisl pretium fusce. Sed arcu non odio euismod lacinia
-            at quis. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.
-            Id faucibus nisl tincidunt eget nullam non nisi est sit. Donec massa sapien faucibus et molestie ac feugiat. Nunc vel risus commodo viverra maecenas
-            accumsan lacus. Arcu bibendum at varius vel pharetra vel turpis. Ultrices gravida dictum fusce ut placerat orci nulla. Habitant morbi tristique
-            senectus et netus. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Vel fringilla est ullamcorper eget nulla facilisi. Felis eget
-            velit aliquet sagittis id consectetur purus ut faucibus. Enim diam vulputate ut pharetra sit amet. Pulvinar proin gravida hendrerit lectus. Lectus
-            vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Convallis
-            posuere morbi leo urna molestie at elementum eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Fames ac turpis egestas sed
-            tempus urna et pharetra. Ullamcorper a lacus vestibulum sed. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Sapien eget mi proin sed
-            libero. Viverra aliquet eget sit amet tellus cras adipiscing enim. Augue eget arcu dictum varius duis. Donec ultrices tincidunt arcu non sodales
-            neque sodales ut etiam. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Nunc eget lorem dolor sed. Iaculis eu non diam phasellus
-            vestibulum lorem sed risus. Tincidunt tortor aliquam nulla facilisi cras fermentum. Hac habitasse platea dictumst vestibulum rhoncus. Accumsan lacus
-            vel facilisis volutpat. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Nibh ipsum consequat nisl vel. Congue quisque
-            egestas diam in arcu cursus euismod quis viverra. Amet luctus venenatis lectus magna fringilla urna. Porta non pulvinar neque laoreet suspendisse
-            interdum. Sit amet justo donec enim diam vulputate ut pharetra sit. Mauris commodo quis imperdiet massa tincidunt. Consequat interdum varius sit
-            amet. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. In nibh mauris cursus mattis. Cras semper auctor neque vitae tempus. Vitae
-            suscipit tellus mauris a diam maecenas. Libero justo laoreet sit amet. Adipiscing diam donec adipiscing tristique risus nec feugiat in. Etiam sit
-            amet nisl purus in. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Ornare aenean euismod elementum nisi quis eleifend quam
-            adipiscing. Magna ac placerat vestibulum lectus. Convallis convallis tellus id interdum velit. Malesuada proin libero nunc consequat interdum. Erat
-            pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Arcu dictum varius duis at consectetur lorem. Sodales ut etiam sit amet nisl
-            purus in mollis nunc. Aliquet eget sit amet tellus cras adipiscing. Amet mattis vulputate enim nulla aliquet porttitor. Eros donec ac odio tempor
-            orci dapibus. Id porta nibh venenatis cras sed felis eget velit aliquet. Nulla pharetra diam sit amet nisl suscipit. Risus sed vulputate odio ut
-            enim blandit volutpat. Elementum integer enim neque volutpat ac tincidunt. Auctor neque vitae tempus quam pellentesque nec nam. In iaculis nunc sed
-            augue lacus viverra vitae congue eu.
+            Let's be honest, by now I've already lost 95% of you and to be really honest I would have lost most of you anyway no matter what I write, so in a
+            manner of good scientist I'm gonna propose a hypothesis which goes something like this:
           </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
+            <i>In order to be concise, one has to employ certain level of abstraction.</i>
+          </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
+            Outlandish? Of course not. But, allow me to explain. Look, we are here in a software development business, so nothing here is a rocket science. We
+            build web apps and web sites, period. You know, apps, these things you interact with through your screen. Or sites, these things you look at, again
+            with agency of your screen. And I expect you have certain expectations. At least I have. It has to be fast, it has to get the job done. It has to be
+            different, but not too much different. It has to be perhaps <i>beautiful?</i> Your clients have to be satisfied with the solution, heck, even Karen
+            in HR has to be little less pissed off while using products we make. And we all know she is pissed off by default just by the fact she has to show
+            up to work every day. And now you tell me how to convey all that jazz in a three letter flashy slogan on the website cover. Tricky, right?
+          </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
+            We have to generalize and we have to differentiate. Not to compare and stuff, but if Bob and Alice (sincerely, kudos to Bob and Alice) are doing
+            their solutions in PHP or in god forbid .Net, we have to try better. Not for the sake of technology per se, because if I loathe one thing biblically
+            & wholeheartedly that is technology/IDE/OS tribalism. But for the sake of you, the consumer. You as a consumer require simply put mission critical
+            solutions that work & scale well if need be and I need platforms which my team and I can deliver that promise on. That is why in most cases I'm
+            going to propose technologies built upon Node.js stack utilizing Jamstack approach and leveraging microservice architecture. Consequently, you are
+            going to get fast page loads, better SEO, seamless scalability and constant possibility of portability and I am going to have great developer
+            experience across my team. Let's not forget I have to juggle between the role of an engineer and the manager role. And even though I enjoy af to
+            nerd out and try new technologies instantly as they become available not in a single moment am I losing site from the big picture and I always know
+            which role is unfortunately more important. All of this in order to make solutions which are better, <i>richer</i> and the journey while using it is
+            pleasant. You know, nice <i>vibe and experience</i>. Right? All of this is an aesthetic in itself.
+          </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
+            However, all of this is nice but we must not get complacent. So after some time maybe I'm going to propose to level up. To utilize power of natural
+            language processing to sort out the mess you probably have in your corporate help desk records in order to be more efficient. Or just simple chatbot
+            for your web shop to "boost that sweet conversion numbers, bro". That is why we need <i>machine learning and neural nets.</i>
+          </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
+            In all honesty, we all know how it works, you pay, we work. Simple as that. No need to uphold meaningless corpo bs. But we genuinely want you to
+            succeed using our solutions and we take great pride in what we do, that is for sure. Sceptic? Legitimately so. You can always ask some of stated
+            businesses on the home page. Maybe it will make you nudge your mind a bit. Regardless, mad love, peace out, y'all.
+          </p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">Siniša Košćec</p>
+          <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">Founder and CEO</p>
         </motion.div>
         <motion.div variants={inputs} className="mx-auto lg:w-1/2 md:w-2/3">
           <div className="flex flex-wrap -m-2">
