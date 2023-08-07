3b0413eab4e73b39cab56aefbbc2f07b764f2e1f
Sinisa Koscec <sinisa@dvasadva.com>
2023-08-07 18:50:35 +0200

    fix even more expertise typos

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -32,7 +32,7 @@ const Expertise = () => {
       />
       <Card
         heading="Large Language Model Inference & Deployment"
-        description="We deploy powerful language models such as LLaMa and Falcon using Hugging Face and LangChain for seamless and efficient AI-driven solutions."
+        description="We deploy powerful language models such as LLaMA and Falcon using Hugging Face and LangChain for seamless and efficient AI-driven solutions."
         color="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
       />
       <Card
