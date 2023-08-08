0dc06cdce7195a17f48057a0343aa8887d08acff
Sinisa Koscec <sinisa@dvasadva.com>
2023-08-08 06:53:12 +0200

    refactor about copy

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     <div className="h-[70vh] bg-teal-500 flex flex-wrap justify-center items-center p-8 lg:p-16 gap-20">
       <div className="basis-80 flex-grow"><p className={`text-3xl md:text-5xl xl:text-6xl 2xl:text-8xl text-zinc-200 ${spaceMono700.className}`}>Interested in what you see?</p></div>
       <div className="basis-80 flex-grow">
-        <div><p className={`text-3xl md:text-5xl xl:text-6xl 2xl:text-8xl text-zinc-200 ${spaceMono700.className}`}>Contact us</p></div>
+        <div><p className={`text-3xl md:text-5xl xl:text-6xl 2xl:text-8xl text-zinc-200 ${spaceMono700.className}`}>Connect at</p></div>
         <div className="my-8"><p className={`text-2xl md:text-4xl 2xl:text-6xl text-zinc-200 ${spaceMono400.className}`}>office@dvasadva.com</p></div>
       </div>
     </div>
--- pages/about.jsx
+++ pages/about.jsx
@@ -27,55 +27,44 @@ const ContactPage = () => {
           <motion.div variants={title} className="flex flex-col w-full mb-12 text-zinc-300">
             <h1 className="mb-4 text-2xl font-medium text-zinc-50 sm:text-3xl title-font text-center">About Us</h1>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              Ok, we all know how it goes. You land on a website of some random business on the Internet run by some random gals and guys and immediately you
-              are slapped by fancy marketing gibberish buzz words such as <i>experiences, rich & digital</i> and of course given the fact we live in 2023
-              <i> machine learning & blazingly fast</i> has to be involved, right?
+              Those humble beginnings in 2016 when dvasadva was incorporated, from today's perspective, seem otherworldly, indeed. It is fair to say that the
+              act of incorporating a business back then was definitely more driven by the force of mindlessness than strategic reasoning. Obviously, sometimes
+              mindlessness really works. But mindlessness will not get you far.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              Hell, I would have thought exactly the same & I really strive to live by the principle of finding niceties in most of situations. At least I'm
-              trying, I'm trying <i>Ringo to be the good shepherd.</i>
+              In order to navigate the treacherous waters of entrepreneurship, vision is essential, combined with an ever-present ability to quickly pivot when
+              circumstances require it.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              Let's be honest, by now I've already lost 95% of you and to be really honest I would have lost most of you anyway no matter what I write, so in a
-              manner of a good scientist I'm gonna propose a hypothesis which goes something like this:
+              With that in mind, and fueled by our passion for creating standout solutions, we began developing web apps and websites using the WordPress
+              architecture. Mind you, that was the time when React and its class-based components (OGs will remember these) were starting to take off.
+              Admittedly, we didn't know much, but we embraced a culture of curiosity and avoided cutting corners – values that we hold dear to this day.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              <i>In order to be concise, one has to employ certain level of abstraction.</i>
+              Soon, the entire WordPress ecosystem began to feel somewhat limiting. As we watched tutorials and online presentations, it became glaringly
+              obvious that React was the new kid on the block. And we all know how it goes with shiny new toys. We dove right in! It was clear from the start
+              that this technology had the potential to revolutionize frontend development, offering clear benefits for both clients and developers. The
+              community was vibrant as well. This is the point when you realize you are part of something impactful and just know that future analyses will
+              prove you right.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              Outlandish? Of course not. But, allow me to explain. Look, we are here in a software development business, so nothing here is rocket science. We
-              build web apps and web sites, period. You know, apps, these things you interact through your screen with. Or sites, these things you look at,
-              again with the agency of your screen. And I expect you to have certain expectations. At least I have. It has to be fast, it has to get the job
-              done. It has to be different, but not too different. It has to be perhaps <i>beautiful?</i> Your stakeholders have to be satisfied with the
-              solution, heck, even Karen in HR has to be little less pissed off while using products we make. And we all know she is pissed off by default just
-              by the fact she has to show up to work every day. And now you tell me how to convey all that jazz in a three letter flashy slogan on the website
-              cover. Tricky, right?
+              This romantic early stage of the technology life cycle obviously can't last long, and immersing oneself in another technology stack is crucial for
+              a much-needed change in perspective. Thus, the strategic decision was made to pivot into the field of machine learning and artificial
+              intelligence. Back in 2020, we speculated that the field would experience a renaissance, but the sheer magnitude of research being done on Large
+              Language Models and the hype measured by how quickly a project can accumulate gigantic volumes of GitHub stars in a short time was honestly beyond
+              our wildest dreams. History rhymes, and it is extremely satisfying to see the potential once again, as well as to be a part of it.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              We have to generalize and we have to differentiate. Not to compare and stuff, but if Bob and Alice (sincerely, kudos to Bob and Alice) are doing
-              their solutions in PHP or in god forbid .Net, we have to try better. Not for the sake of technology per se, because if I loathe one thing
-              biblically & wholeheartedly that is technology/IDE/OS tribalism. But for the sake of you, the consumer. You as a consumer require simply put
-              mission critical solutions that work & scale well if need be and I need platforms which my team and I can deliver that promise on. That is why in
-              most cases I'm going to propose technologies built upon Node.js stack utilizing Jamstack approach and leveraging microservice architecture.
-              Consequently, you are going to get fast page loads, better SEO, seamless scalability and constant possibility of portability and I am going to
-              have great developer experience across my team. Let's not forget I have to juggle between the role of an engineer and the manager role. And even
-              though I enjoy af to nerd out and try new technologies instantly as they become available, not in a single moment am I losing sight from the big
-              picture and I always know which role is unfortunately more important. All of this in order to make solutions which are better, <i>richer</i> and
-              the journey while using it is pleasant. You know, nice <i>vibe and experience</i>. Right? All of this is an aesthetic in itself.
+              With our feet firmly planted on the ground, a human-centric approach, and a commitment to constructing solutions that benefit people throughout
+              the production chain, we prioritize these values in every aspect of our business. In our case, the code is written for people, not for machines,
+              and it should be easily understandable even if it means being a bit verbose.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              However, all of this is nice but we must not get complacent. So after some time maybe I'm going to propose to level up. To utilize power of
-              natural language processing to sort out the mess you probably have in your corporate help desk records in order to be more efficient. Or just
-              simple chatbot for your web shop to "boost that sweet conversion numbers, bro". That is why we need <i>machine learning and neural nets.</i>
+              Throughout our business journey, honesty and a straightforward approach have always been paramount pillars that we have abided by, alongside our
+              strong business acumen.
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              In all honesty, we all know how it works, you pay, we work. Simple as that. No need to uphold meaningless corpo bs. But we genuinely want you to
-              succeed using our solutions and we take great pride in what we do, that is for sure. Sceptic? Legitimately so. You can always consult with some of
-              the stated businesses on the home page on our performance and ultimately were they satisfied with us. Maybe it will make you nudge your mind a
-              bit. Regardless, mad love, peace out, y'all.
-            </p>
-            <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">
-              If you have come this far, I humbly thank you. It is a pleasure to have you right here. Peace!
+              If you've come this far, I humbly thank you. I truly do. It's a pleasure to have you right here. Peace!
             </p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">Siniša Košćec</p>
             <p className="mx-auto my-3 text-base leading-relaxed lg:w-2/3">Founder and CEO</p>
